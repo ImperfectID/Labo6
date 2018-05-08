2e60323c309960ba3f18eee6ce9187b2ac23eabc
uca <uca@uca.com>
2018-05-08 12:30:56 -0600

    labo

---
+++ app/src/main/java/com/meag/labo6/MainActivity.java
@@ -0,0 +1,32 @@
+package com.meag.labo6;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDrawerLayout = findViewById(R.id.drawerLayout);
+
+        NavigationView navigationView = findViewById(R.id.navegationView);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                mDrawerLayout.closeDrawers();
+                
+
+                return true;
+            }
+        });
+
+    }
+}
